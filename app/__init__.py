e76e67375238ea2e49d5a3a2c6cf7231f8627001
Roma Sytsevich <sytsevichmain@gmail.com>
2026-01-19 13:49:29 +0000

    Add database, migrations, User model

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from .config import Config
 from .routes.main import main_bp
+from .extensions import db, migrate
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     app.register_blueprint(main_bp)
 
--- app/config.py
+++ app/config.py
@@ -1,2 +1,8 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 class Config:
     SECRET_KEY = "dev-secret-key"
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{BASE_DIR / 'skilltracker.db'}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
